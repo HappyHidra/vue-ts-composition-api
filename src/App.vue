f34736e03ee4afc2879b1af30704ea1e0e8cf155
DeepReap <okmmiha@mail.ru>
2023-07-05 13:21:56 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import GameHeader from '@/components/GameHeader.vue'
+import GameFigure from '@/components/GameFigure.vue'
+import GameWrongLetters from '@/components/GameWrongLetters.vue'
+import GameWord from '@/components/GameWord.vue'
+import GamePopup from '@/components/GamePopup.vue'
+import GameNotification from '@/components/GameNotification.vue'
+import { ref } from 'vue'
+
+const word = ref()
+</script>
+
+<template>
+  <div id="app">
+    <GameHeader />
+    <!-- Game container -->
+    <div class="game-container">
+      <GameFigure />
+      <GameWrongLetters />
+      <GameWord />
+    </div>
+    <!-- Container for final message -->
+    <GamePopup v-if="false" />
+    <!-- Notification -->
+    <GameNotification />
+  </div>
+</template>
